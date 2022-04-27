5d1473d6df7f95aca0e6e287b9544f7866f83cd1
Bruno Bartolomasi <brunobrt@hotmail.com>
2022-04-27 19:48:36 -0300

    rename storage class and methods

--- lib/persistence/file_handler.dart
+++ lib/persistence/file_handler.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
 
-class CounterStorage {
+class FileHandler {
   Future<String> get _localPath async {
     final directory = await getApplicationDocumentsDirectory();
 
@@ -26,10 +26,9 @@ class CounterStorage {
     }
   }
 
-  Future<File> writeCounter(int counter) async {
+  Future<File> writeMovies(String cpdMovies) async {
     final file = await _localFile;
 
-    // Write the file
-    return file.writeAsString('$counter');
+    return file.writeAsString('$cpdMovies');
   }
 }
